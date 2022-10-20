9eb84f99da331770d1b270a36a0686ed84aa5014
John Sandalis <giannis.sandalis@gmail.com>
2022-10-20 21:05:17 +0300

    Add js for sticky nav, smooth scrolling & add improvements

--- js/main.js
+++ js/main.js
@@ -3,7 +3,78 @@ const btnNavEl = document.querySelector(".nav__btn");
 const faqButtons = document.querySelectorAll(
   "button[aria-expanded][aria-controls]"
 );
+// Mobile navigation
+const overlay = document.querySelector(".overlay");
+const mobileNav = function () {
+  btnNavEl.addEventListener("click", function () {
+    headerEl.classList.toggle("nav-open");
+    overlay.classList.toggle("hidden");
+  });
+};
 
+const closeNav = function () {
+  headerEl.classList.remove("nav-open");
+  overlay.classList.add("hidden");
+};
+
+overlay.addEventListener("click", function () {
+  closeNav();
+});
+
+// Sticky nav
+const heroEl = document.querySelector(".hero-section");
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) {
+    headerEl.classList.add("sticky");
+    headerEl.classList.add("fadeInTop");
+  } else {
+    headerEl.classList.remove("fadeInTop");
+  }
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: "-61px",
+});
+
+const observeNav = function () {
+  window.addEventListener("scroll", () => {
+    headerEl.classList.contains("sticky") && window.scrollY <= 80
+      ? headerEl.classList.remove("sticky")
+      : "";
+
+    headerEl.classList.contains("nav-open") ? closeNav() : "";
+  });
+};
+
+// Smooth link scrolling
+const linksList = document.querySelectorAll(".nav__list--link");
+const sectionsList = document.querySelectorAll("section");
+
+const smoothScrolling = function () {
+  linksList.forEach((link) => {
+    if (link.name === "hero") {
+      link.addEventListener("click", function (e) {
+        e.preventDefault();
+        window.scrollTo({ top: 0, behavior: "smooth" });
+      });
+    } else {
+      link.addEventListener("click", function (e) {
+        e.preventDefault();
+        sectionsList.forEach((section) => {
+          section.id === link.name
+            ? section.scrollIntoView({ behavior: "smooth" })
+            : "";
+        });
+      });
+    }
+  });
+};
+
+// FAQ Section
 class DisclosureButton {
   constructor(buttonNode) {
     this.buttonNode = buttonNode;
@@ -64,14 +135,19 @@ window.addEventListener(
   "load",
   function () {
     // Mobile navigation
-    btnNavEl.addEventListener("click", function () {
-      headerEl.classList.toggle("nav-open");
-    });
+    mobileNav();
+
+    // Sticky Nav
+    headerObserver.observe(heroEl);
+    observeNav();
+
+    // Smooth Scrolling
+    smoothScrolling();
 
     // FAQ Buttons
-    for (var i = 0; i < faqButtons.length; i++) {
-      new DisclosureButton(faqButtons[i]);
-    }
+    faqButtons.forEach((faqButton) => {
+      new DisclosureButton(faqButton);
+    });
   },
   false
 );
